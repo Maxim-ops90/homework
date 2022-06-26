1eef30fd776266fc2e7479563266578a22d0bd57
Maxim Mineev <biton.90@mail.ru>
2022-06-26 18:32:10 +0300

    исправление в 6 задачи

--- task_006/Program.cs
+++ task_006/Program.cs
@@ -7,5 +7,5 @@
 
 Console.WriteLine("Введите целое число");
 int num = Convert.ToInt32(Console.ReadLine());
-if (num % 2 == 0) Console.WriteLine("Четное");
-else Console.WriteLine("Нечетное");
+if (num % 2 == 0) Console.WriteLine("Число является четным");
+else Console.WriteLine("Число не является четным");
