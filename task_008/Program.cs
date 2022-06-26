4eeaa069e4a1f6e1a0b430d8d23fcd3fef767895
Maxim Mineev <biton.90@mail.ru>
2022-06-26 19:58:01 +0300

    task_008 last

--- task_008/Program.cs
+++ task_008/Program.cs
@@ -2,4 +2,17 @@
 // а на выходе показывает все чётные числа от 1 до N.
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
+Console.WriteLine("Введите целое число N:");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+while (true)
+{
+    if (i % 2 == 0)
+        Console.Write(i + ",");
+    i++;
+    {
+        if (i > N)
+            break;
+    }
+}
 
