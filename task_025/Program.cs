e1d1f0a0e2f162c64df6399a2e806d1975324b6a
Maxim Mineev <biton.90@mail.ru>
2022-07-10 10:27:38 +0300

    сделал дз

---
+++ task_025/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+
+Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int Pow(int a,int b)
+{
+
+    int pow = 1;
+    for (int i = 0; i < b; i++)
+    {
+        pow *= a;
+    }
+    return pow;
+}
+
+int powA = Pow(num1,num2);
+Console.WriteLine($"Число {num1} в степени {num2} равно {powA}");
---
+++ task_027/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int SumNumber(int number)
+{
+    int summa = 0;
+    while (number > 0)
+    {
+        summa += number % 10;
+        number = number / 10;
+    }
+    return summa;
+}
+
+int sum = SumNumber(num);
+Console.WriteLine($"Сумма цифр в числе {num} равна {sum}");
